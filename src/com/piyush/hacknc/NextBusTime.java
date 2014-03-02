067ec91b66db9b972a1f9cb7f1c2a6ba91cec80d
Rohit <rkgupta@live.unc.edu>
2014-03-02 05:48:33 -0500

    Revert "Revert "update_rohit""
    This reverts commit be1de7a7819b7e84bb8bace57f0354519730636b.

--- src/com/piyush/hacknc/NextBusTime.java
+++ src/com/piyush/hacknc/NextBusTime.java
@@ -2,9 +2,9 @@ package com.piyush.hacknc;
 
 public class NextBusTime {
 	
-	private String busName;
-	private int eta;
-	private String stopName;
+	private String busName  = "CM";
+	private int eta = 12;
+	private String stopName = "SN";
 	
 	public String getBusName() {
 		return busName;
